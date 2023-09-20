5e68146ee8e9af23f5ba3aa167d3a95398122f12
名執陸 <RIKUNATORI@youchikaojisannoiMac.local>
2023-09-20 16:24:54 +0900

    feat: installed firestore in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -33,6 +44,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    
 
     return Scaffold(
       body: Column(
@@ -40,8 +52,17 @@ class MyHomePage extends StatelessWidget {
           Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
           ElevatedButton(
-            onPressed: () {
+            onPressed: () async {
               print('button pressed!');
+              await FirebaseFirestore.instance
+                .collection('test_table')
+                .doc('test_doc')
+                .set({
+                  'id': '1',
+                  'num': 4,
+                  'str': "test",
+                  'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
+                });
             },
             child: Text('Next'),
           ),
