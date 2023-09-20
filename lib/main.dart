5b94fc02931487d3768db588290bad207e2587e4
rare0b <rare0b0110@gmail.com>
2023-09-20 16:32:29 +0900

    Revert "feat: installed firestore in flutter"
    This reverts commit 5e68146ee8e9af23f5ba3aa167d3a95398122f12.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
-import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'footer.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(MyApp());
 }
 
@@ -45,7 +34,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    
 
     return Scaffold(
       body: Column(
@@ -53,17 +41,8 @@ class MyHomePage extends StatelessWidget {
           Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
           ElevatedButton(
-            onPressed: () async {
+            onPressed: () {
               print('button pressed!');
-              await FirebaseFirestore.instance
-                .collection('test_table')
-                .doc('test_doc')
-                .set({
-                  'id': '1',
-                  'num': 4,
-                  'str': "test",
-                  'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
-                });
             },
             child: Text('Next'),
           ),
